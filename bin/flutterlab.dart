7f95997356297996a1776af9507ecc0ff65e551c
shirodeep <=shirodeepdahal0987@gmail.com>
2023-02-21 03:55:37 +0545

    maa bhat khancu tme pani deu maa pass hanxu tme tani deu

---
+++ bin/flutterlab.dart
@@ -0,0 +1,5 @@
+import 'package:flutterlab/flutterlab.dart' as flutterlab;
+import 'dart:io';
+void main() {
+
+}
---
+++ test/flutterlab_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutterlab/flutterlab.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
